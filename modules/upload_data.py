8589a34ca3ed88b56375ddb9c7569427a0d5d592
Brandon Selbig <156852197+selbigmtnwx23@users.noreply.github.com>
2025-02-25 15:29:33 -0700

    Update upload_data.py
    tweak for coastal profile

--- modules/upload_data.py
+++ modules/upload_data.py
@@ -50,7 +50,7 @@ class UploadData():
             self.bucket_name = 'noaa-ufs-regtests-pds'
             self.profile = 'ufs-wm-rt-app'
         elif use bucket == 'coastal':
-            self.bucket_name = 'noaa-ufs-costal-pds'
+            self.bucket_name = 'noaa-ufs-coastal-pds'
             self.profile = 'coastal-app'
         else:
             print(f"{use_bucket} Bucket Does Not Exist.")
